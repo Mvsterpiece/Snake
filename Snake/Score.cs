f166f5d0efe65477e6d472bdbbc36ed34306d056
Denis Saiko <93324253+Mvsterpiece@users.noreply.github.com>
2022-03-23 14:41:49 +0200

    Update TTHK

--- Snake/Score.cs
+++ Snake/Score.cs
@@ -1,30 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Snake
 {
-	class Score
+	public class Score
 	{
         private int score;
+        public int level;
         public int speed;
-        public Score(int score)
+        public Score(int score, int level)
         {
             this.score = score;
+            this.level = level;
         }
-        public bool ScoreUp()//Увеличение Score
+        public void ScoreUp()//Увеличение Score
         {
-            score += 1;
-            if (score % 10 == 0)//Обозначние Score на 1 уровень
-            {
-                return true;
-            }
-            else { return false; }
-        }
-        public void ScoreWrite()
-        {
-            Console.SetCursorPosition(70, 1);
+            Console.SetCursorPosition(81, 12);
             Console.WriteLine("Score:" + score.ToString());
+            score++;
         }
     }
 }
